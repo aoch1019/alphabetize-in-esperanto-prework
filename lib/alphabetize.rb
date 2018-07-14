8c1c9cf2079dd2805e691a7b853aec72888cd2c0
Andrew Och <andrewoch95@gmail.com>
2018-07-14 15:19:09 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_array = ESPERANTO_ALPHABET.split
   
+  esp_hash = {}
+  
+  esp_array.each_with_index do |letter, index|
+    esp_hash[letter] = index
+  end
+  binding.pry
 end
